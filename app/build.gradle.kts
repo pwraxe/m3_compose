b2fd6a0fda2fc615175fd3e31adf1bfc58e8b8d8
Akshay <pawarakshay13@gmail.com>
2025-08-02 22:46:55 +0530

    version upgraded

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.codexdroid.m3compose"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
